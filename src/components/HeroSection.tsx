be51c366bed6038f996905cea647f18c0593d1e4
saifalissaiyed10-cmd <saifalis.saiyed10@gmail.com>
2026-04-07 18:32:50 +0530

    reduced the top padding of the main content in the Hero section so that it sits a little higher up and closer to the navbar, reducing the empty space!

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -524,7 +524,7 @@ export default function HeroSection() {
       <FloatingParticles />
       <Navbar />
 
-      <div className="relative z-20 mx-auto flex flex-1 w-full max-w-7xl flex-col justify-center px-6 pt-20 pb-20 lg:flex-row lg:items-center lg:px-12 lg:pt-20 lg:pb-32">
+      <div className="relative z-20 mx-auto flex flex-1 w-full max-w-7xl flex-col justify-start px-6 pt-24 pb-10 lg:flex-row lg:items-start lg:px-12 lg:pt-24 lg:pb-10">
         
         {/* LEFT COLUMN */}
         <div className="flex w-full flex-col justify-center lg:w-[50%]">
